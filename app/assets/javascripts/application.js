6258adfa1341c90ac730061000c37dc5d0fe783e
David Tatti <david@swarmplanet.com>
2014-01-14 14:47:05 +0100

    fixed asset pipeline and static serving for production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+// require_tree .
+//= require tools/jquery-1.7.1
+//= require tools/jquery.cookie
+//= require tools/jquery.base64.js
+//= require ../oceanfront/oceanfront
+//= require common
+//= require @js_app_name 
