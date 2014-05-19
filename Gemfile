7d7325fc7c850a355184f0b0c371ca9600e0c4b3
4lt4i <github@altai.se>
2014-05-19 11:20:51 +0200

    Spec_helper and Gemfile are now configurable with the bundle install --without default test development and setting CLIENT_HOST in a way that disables rails when running the frontend tests.

--- Gemfile
+++ Gemfile
@@ -1,26 +1,31 @@
 source 'https://rubygems.org'
 
-gem "rails", "~> 4.0.0"
-gem "ocean-rails", ">= 2.11.2 "
-gem "ocean-dynamo", ">= 0.3.1"
-
-gem 'net-purge'          # For Varnish purges
-gem 'rack-attack'        # HTTP Whitelist/Blacklist and Request Rate Limiter for Rack Applications
-gem 'faraday'            # We use Faraday to make JSON requests
-gem 'faraday_middleware' # Useful for parsing JSON responses, etc.
-
-gem 'bcrypt-ruby', :require => 'bcrypt'       # Password hashing, etc
+group :default do
+  gem "rails", "~> 4.0.0"
+  gem "ocean-rails", ">= 2.11.2 "
+  gem "ocean-dynamo", ">= 0.3.1"
+  gem 'net-purge'          # For Varnish purges
+  gem 'rack-attack'        # HTTP Whitelist/Blacklist and Request Rate Limiter for Rack Applications
+  gem 'faraday'            # We use Faraday to make JSON requests
+  gem 'faraday_middleware' # Useful for parsing JSON responses, etc.
+  gem 'bcrypt'
+  gem 'uglifier', '>= 1.3.0'
+  gem 'oj'
+end 
 
 group :test, :development do
   gem "rspec-rails", "~> 2.0"
   gem "simplecov", :require => false
-  gem "multi_json",  "~> 1.0"                 # Needed by selenium-webdriver (but not required??)
   gem "selenium-webdriver"
-  #gem "watir-rails"
   gem "watir-webdriver-rails"
-  gem "ffi", "= 1.9.0"
+  gem "ffi"
   gem "factory_girl_rails", "~> 4.0"
-  gem "test-unit", ">= 2.5.5"
 end
 
-gem 'uglifier', '>= 1.3.0'
+group :windows do
+  gem "ffi"
+  gem "selenium-webdriver"
+  gem "watir-webdriver"
+  gem "rspec"
+  gem "rake"
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,99 +1,79 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'development'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
-require 'watir-webdriver-rails'
+# Set up the host and port of the client app being tested 
+client_host = ENV["CLIENT_HOST"] || "http://localhost"
+client_port = ENV["CLIENT_PORT"] || 3000 
+
+# Setting CLIENT_HOST indicates that you want to test against
+# an external server, and therefore not use rails on windows
+if ENV["CLIENT_HOST"]
+  require "watir-webdriver"
+  require "test/unit"
+  include Test::Unit::Assertions
+else
+  ENV["RAILS_ENV"] ||= 'development'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+  require 'watir-webdriver'
 
 
-# Custom drag_and_drop method for Watir
-module Watir
-  class Element
-    def drag_and_drop_on(other)
-      assert_exists
-      driver.action.drag_and_drop(@element, other.wd).perform
+  # Custom drag_and_drop method for Watir
+  module Watir
+    class Element
+      def drag_and_drop_on(other)
+        assert_exists
+        driver.action.drag_and_drop(@element, other.wd).perform
+      end
     end
   end
-end
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  #config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  #config.use_transactional_fixtures = true
-
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = "random"
-end
-
-
-# Set up the host and port of the client app being tested 
-client_host = ENV['CLIENT_HOST'] || "http://localhost"
-client_port = ENV['CLIENT_PORT'] || 3000
-
-# Set up the user and password of the user used to log in to perform the tests
-TEST_API_USER = ENV['TEST_API_USER'] || TEST_API_USER
-
-tapw = ENV['TEST_API_PASSWORD'] || TEST_API_PASSWORD
-master, staging = tapw.split(',')
-tapw = (ENV['GIT_BRANCH'] == 'staging' ? staging : master) if staging.present?
-TEST_API_PASSWORD = tapw
-
-# Configure Watir
-WatirWebdriverRails.host = client_host
-WatirWebdriverRails.port = client_port
-WatirWebdriverRails.close_browser_after_finish = true
-
-URL = "#{client_host}:#{client_port}"
-
-def setup_browser(uri)
-  if RUBY_PLATFORM =~ /linux/
-    @headless = Headless.new
-    @headless.start
-    b = Watir::Browser.start uri
-  else
-    b = Watir::Browser.new ENV["browser"] || :chrome
-    b.goto uri
+  RSpec.configure do |config|
+    config.infer_base_class_for_anonymous_controllers = false
+    config.order = "random"
   end
 
-  # Make sure that window is maximized to not get viewport errors
-  screen_width = b.execute_script("return screen.width;")
-  screen_height = b.execute_script("return screen.height;")
-  b.driver.manage.window.resize_to(screen_width,screen_height)
-  b.driver.manage.window.move_to(0,0)
-  b
-end
+  # Set up the user and password of the user used to log in to perform the tests
+  TEST_API_USER = ENV['TEST_API_USER'] || TEST_API_USER
+
+  tapw = ENV['TEST_API_PASSWORD'] || TEST_API_PASSWORD
+  master, staging = tapw.split(',')
+  tapw = (ENV['GIT_BRANCH'] == 'staging' ? staging : master) if staging.present?
+  TEST_API_PASSWORD = tapw
+
+  # Configure Watir
+  WatirWebdriverRails.host = client_host
+  WatirWebdriverRails.port = client_port
+  WatirWebdriverRails.close_browser_after_finish = true
+
+  URL = "#{client_host}:#{client_port}"
+
+  def setup_browser(uri)
+    if RUBY_PLATFORM =~ /linux/
+      @headless = Headless.new
+      @headless.start
+      b = Watir::Browser.start uri
+    else
+      b = Watir::Browser.new ENV["browser"] || :chrome
+      b.goto uri
+    end
+
+    # Make sure that window is maximized to not get viewport errors
+    screen_width = b.execute_script("return screen.width;")
+    screen_height = b.execute_script("return screen.height;")
+    b.driver.manage.window.resize_to(screen_width,screen_height)
+    b.driver.manage.window.move_to(0,0)
+    b
+  end
 
 
-def teardown_browser(browser)
-  if RUBY_PLATFORM =~ /linux/
-    #@headless.take_screenshot "miffo-#{rand(1000000000)}.jpg"
-    browser.close
-    @headless.destroy
-  else
-    browser.close
+  def teardown_browser(browser)
+    if RUBY_PLATFORM =~ /linux/
+      #@headless.take_screenshot "miffo-#{rand(1000000000)}.jpg"
+      browser.close
+      @headless.destroy
+    else
+      browser.close
+    end
   end
 end
