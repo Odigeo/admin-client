a86a8bdcc425223c1ef1b288bed27cd3699b4e40
David Tatti <lespaul80@hotmail.com>
2014-06-17 17:06:17 +0200

    removed cms test until cms is up and running

--- spec/requests/cms_spec.rb
+++ spec/requests/cms_spec.rb
@@ -1,32 +1,32 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe "cms" do
+# describe "cms" do
 
-   after do
-     teardown_browser(@b)
-   end
+#    after do
+#      teardown_browser(@b)
+#    end
   
-   def do_login
-     @b = setup_browser URL+"/cms"
-     assert(@b.text_field(:id, "login-input"))
-       @b.text_field(:id, "login-input").wait_until_present
-       @b.text_field(:id, "login-input").set(TEST_API_USER)
-       @b.text_field(:id, "password-input").set(TEST_API_PASSWORD)
-       #@b.send_keys :enter
-       @b.div(:id, "login-confirm-button").click
-       # Wait until we can see input elements for search
-       @b.text_field(:id, "app").wait_until_present
-     rescue => e
-   end
+#    def do_login
+#      @b = setup_browser URL+"/cms"
+#      assert(@b.text_field(:id, "login-input"))
+#        @b.text_field(:id, "login-input").wait_until_present
+#        @b.text_field(:id, "login-input").set(TEST_API_USER)
+#        @b.text_field(:id, "password-input").set(TEST_API_PASSWORD)
+#        #@b.send_keys :enter
+#        @b.div(:id, "login-confirm-button").click
+#        # Wait until we can see input elements for search
+#        @b.text_field(:id, "app").wait_until_present
+#      rescue => e
+#    end
 
-   it "should be able to search for a application" do
-     do_login
-     @b.text_field(:id, "app").set("test")
-     @b.send_keys :enter
-     @b.div(:class, "cms-card-panel").wait_until_present
-     @b.div(:class, "cms-card-panel").div(:class, "closeButton").exists?
-       @b.div(:class, "cms-card-panel").div(:class, "closeButton").click
-   end
+#    it "should be able to search for a application" do
+#      do_login
+#      @b.text_field(:id, "app").set("test")
+#      @b.send_keys :enter
+#      @b.div(:class, "cms-card-panel").wait_until_present
+#      @b.div(:class, "cms-card-panel").div(:class, "closeButton").exists?
+#        @b.div(:class, "cms-card-panel").div(:class, "closeButton").click
+#    end
 
   # it "should be able to search and read a text object" do
   #   do_login
@@ -70,4 +70,4 @@ describe "cms" do
   #   @b.div(:class => "cms-object", :index => 1).text_field(:placeholder, "Image tags").value.should == "blond, woman"
   # end
 
-end
+#end
